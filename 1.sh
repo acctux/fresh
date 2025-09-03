1abbda260650f657353d3e9f5baad5a42201a860
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-03 12:27:51 -0400

    e
    e

--- 1.sh
+++ 1.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo mkdir -p /mnt/usb
-sudo mount /dev/sdb1 /mnt/usb
-cd /mnt/usb
-mkdir -p "$HOME/.ssh"
-cp -r "ssh" "$HOME/.ssh"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-cp wifi.sh "$HOME"
+device=$(lsblk -o NAME,TYPE -nr | grep 'part' | grep -v '^sda' | awk '{print "/dev/" $1}' | head -n 1)
+musb="/mnt/usb"
+
+if [ -n "$device" ]; then
+    sudo mkdir -p "$musb"
+    sudo mount "$device" "$musb"
+    echo "Mounted $device to $musb"
+else
+    echo "No suitable USB device found."
+fi
+
+if [ -d "$musb" ]; then
+    cd $musb
+    mkdir -p "$HOME/.ssh"
+    cp -r "ssh" "$HOME/.ssh"
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519
+    cp wifi.sh "$HOME"
+else
+    echo "No USB folder found."
+fi
+
 ./2.sh & exit 0

e70296a9ea46ddb6b6b642f59317870b2848bb16
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-10 03:19:56 +0300

    bnm
    bnm

--- lib/dotfiles.sh
+++ lib/dotfiles.sh
@@ -50,42 +50,8 @@ copy_system_config() {
     sudo chmod 440 /etc/sudoers.d/mysudo
 }
 
-manage_services() {
-    log INFO "Managing services..."
-    for service in "${SERVENABLE[@]}"; do
-        systemctl list-unit-files "$service.service" &>/dev/null &&
-            sudo systemctl enable "$service" && log INFO "Enabled $service" ||
-            log WARNING "Service $service not found."
-    done
-    for service in "${SERVDISABLE[@]}"; do
-        systemctl list-unit-files "$service.service" &>/dev/null &&
-            sudo systemctl disable "$service" && log INFO "Disabled $service" ||
-            log WARNING "Service $service not found."
-    done
-    systemctl --user enable pipewire pipewire-pulse wireplumber ssh-agent 2>/dev/null ||
-        log WARNING "Failed to enable user services."
-}
-
-cleanup_files() {
-    log INFO "Cleaning up files..."
-    for item in "${CLEANUP_USER_FILES[@]}"; do
-        [[ -e "$item" ]] && rm -rf "$item" && log INFO "Removed $item" ||
-            log WARNING "Item $item not found."
-    done
-    for file in "${CLEANUP_SYS_FILES[@]}"; do
-        [[ -f "$file" ]] && sudo rm -f "$file" && log INFO "Removed $file" ||
-            log WARNING "File $file not found."
-    done
-    for dir in "${CLEANUP_SYS_DIRS[@]}"; do
-        [[ -d "$dir" ]] && sudo rm -rf "$dir" && log INFO "Removed $dir" ||
-            log WARNING "Directory $dir not found."
-    done
-}
-
 setup_dotfiles_and_config() {
-    backup_existing_dotfiles
+    backup_dotfiles_dry_run
     stow_dotfiles
     copy_system_config
-    manage_services
-    cleanup_files
 }

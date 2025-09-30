a3c52780bef30e83a2199bda6eb4111a8d1d04b8
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-30 19:21:56 -0400

    Update fresh.sh

--- fresh/fresh.sh
+++ fresh/fresh.sh
@@ -1,9 +1,8 @@
-
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm
 pacman -S --noconfirm --needed git
 rm -rf ~/fresh
 git clone https://github.com/acctux/fresh.git ~/fresh
-chmod +x fresh/fresh/fresh.sh
-fresh/fresh/fresh.sh
+chmod +x fresh/fresh/main.sh
+fresh/fresh/main.sh

ca77a9a0f7d0c6c49e52a46f2a0102df01c051f2
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-30 18:58:20 -0400

    seems okay-ish, needs more debug but this is close

--- fresh/fresh.sh
+++ fresh/fresh.sh
@@ -3,6 +3,7 @@ pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm
 pacman -S --noconfirm --needed git
-git clone https://github.com/acctux/fresh.git
+rm -rf ~/fresh
+git clone https://github.com/acctux/fresh.git ~/fresh
 chmod +x fresh/fresh/fresh.sh
 fresh/fresh/fresh.sh

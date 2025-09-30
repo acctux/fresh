01fdbe695caf089e41820caedf3b60c318d9104a
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-30 18:27:08 -0400

    seperating -Sy update and git was enough, now getting it to run the rest

--- fresh/fresh.sh
+++ fresh/fresh.sh
@@ -3,10 +3,6 @@ pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm
 pacman -S --noconfirm git
-if ! git clone https://github.com/acctux/fresh.git; then
-    pacman-key --refresh-keys
-    pacman -S --noconfirm git
-    git clone https://github.com/acctux/fresh.git
-fi
-cd fresh/fresh
-./fresh.sh
+git clone https://github.com/acctux/fresh.git
+chmod +x fresh/fresh/fresh.sh
+fresh/fresh/fresh.sh

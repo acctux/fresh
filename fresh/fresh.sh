492d9c564459fcc8938a53f220c909fce481ae7e
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-30 18:19:04 -0400

    not sure why it's still trying to confirm

--- fresh/fresh.sh
+++ fresh/fresh.sh
@@ -1,7 +1,8 @@
 
 pacman-key --init
 pacman-key --populate archlinux
-pacman -Sy --noconfirm git
+pacman -Sy --noconfirm
+pacman -S --noconfirm git
 git clone https://github.com/acctux/fresh.git
 cd fresh/fresh
 ./fresh.sh

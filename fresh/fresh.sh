3e03fc59e5207a475754c876f98a2c3ef9602394
Nick A <226946345+acctux@users.noreply.github.com>
2025-09-30 18:23:35 -0400

    debug with exit exception

--- fresh/fresh.sh
+++ fresh/fresh.sh
@@ -3,6 +3,10 @@ pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm
 pacman -S --noconfirm git
-git clone https://github.com/acctux/fresh.git
+if ! git clone https://github.com/acctux/fresh.git; then
+    pacman-key --refresh-keys
+    pacman -S --noconfirm git
+    git clone https://github.com/acctux/fresh.git
+fi
 cd fresh/fresh
 ./fresh.sh

4fd194a2767a3a94db23641d6fc0376eb3e0e34f
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-03 17:20:42 -0400

    escape the pipe

--- 1.sh
+++ 1.sh
@@ -2,4 +2,4 @@ pacman -Sy --noconfirm archlinux-keyring
 rm -rf ~/fresh
 pacman -Sy --noconfirm --needed git
 git clone https://github.com/acctux/fresh.git ~/fresh
-exec ~/fresh/ark.sh
+exec bash ~/fresh/ark.sh

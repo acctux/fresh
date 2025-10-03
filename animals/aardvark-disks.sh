1f0b8082deed98236b29c63393cda5d397467c4e
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-02 20:32:03 -0400

    disks 2

--- animals/aardvark-disks.sh
+++ animals/aardvark-disks.sh
@@ -146,16 +146,19 @@ partition_prefix() {
 
 create_partitions() {
     info "Creating partitions on $DISK"
-    wipefs -af "$DISK"
-    sgdisk --zap-all "$DISK" >/dev/null
-    sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' "$DISK"
-    sgdisk -n 2::+${EFI_SIZE} --typecode=2:ef00 --change-name=2:'EFIBOOT' "$DISK"
-    sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' "$DISK"
-    partprobe "$DISK"
-    until lsblk "$DISK" | grep -q "part"; do
-        sleep 0.5
-    done
-    success "Partitions created successfully"
+    umount -A --recursive /mnt # make sure everything is unmounted before we start
+    # disk prep
+    sgdisk -Z ${DISK} # zap all on disk
+    sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+    # create partitions
+    sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+    sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+    sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+    if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+        sgdisk -A 1:set:2 ${DISK}
+    fi
+    partprobe ${DISK}
 }
 
 format_partitions() {

8a76e69f84abad8fa36088716088f0af9fa9ab60
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-02 23:43:33 -0400

    I already know this menu works

--- animals/aardvark-disks.sh
+++ animals/aardvark-disks.sh
@@ -1,148 +1,37 @@
 #!/bin/bash
-select_option() {
+get_disk_selection() {
+    info "Detecting available disks..."
+    local disks=()
+    local labels=()
 
-    # little helpers for terminal print control and key input
-    ESC=$( printf "\033")
-    cursor_blink_on()  { printf "$ESC[?25h"; }
-    cursor_blink_off() { printf "$ESC[?25l"; }
-    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
-    print_option()     { printf "$2   $1 "; }
-    print_selected()   { printf "$2  $ESC[7m $1 $ESC[27m"; }
-    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
-    get_cursor_col()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${COL#*[}; }
-    key_input()         {
-                        local key
-                        IFS= read -rsn1 key 2>/dev/null >&2
-                        if [[ $key = ""      ]]; then echo enter; fi;
-                        if [[ $key = $'\x20' ]]; then echo space; fi;
-                        if [[ $key = "k" ]]; then echo up; fi;
-                        if [[ $key = "j" ]]; then echo down; fi;
-                        if [[ $key = "h" ]]; then echo left; fi;
-                        if [[ $key = "l" ]]; then echo right; fi;
-                        if [[ $key = "a" ]]; then echo all; fi;
-                        if [[ $key = "n" ]]; then echo none; fi;
-                        if [[ $key = $'\x1b' ]]; then
-                            read -rsn2 key
-                            if [[ $key = [A || $key = k ]]; then echo up;    fi;
-                            if [[ $key = [B || $key = j ]]; then echo down;  fi;
-                            if [[ $key = [C || $key = l ]]; then echo right;  fi;
-                            if [[ $key = [D || $key = h ]]; then echo left;  fi;
-                        fi
-    }
-    print_options_multicol() {
-        # print options by overwriting the last lines
-        local curr_col=$1
-        local curr_row=$2
-        local curr_idx=0
+    while IFS= read -r line; do
+        line="${line% disk}"
+        local name size model
+        name=$(awk '{print $1}' <<< "$line")
+        size=$(awk '{print $2}' <<< "$line")
+        model=$(cut -d' ' -f3- <<< "$line")
+        if [[ -b "/dev/$name" ]]; then
+            disks+=("/dev/$name")
+            labels+=("$name ($size) - $model")
+        fi
+    done < <(lsblk -dn -o NAME,SIZE,MODEL,TYPE | grep 'disk$')
 
-        local idx=0
-        local row=0
-        local col=0
+    if [[ ${#disks[@]} -eq 0 ]]; then
+        fatal "No suitable disks found"
+    fi
 
-        curr_idx=$(( $curr_col + $curr_row * $colmax ))
-
-        for option in "${options[@]}"; do
-
-            row=$(( $idx/$colmax ))
-            col=$(( $idx - $row * $colmax ))
-
-            cursor_to $(( $startrow + $row + 1)) $(( $offset * $col + 1))
-            if [ $idx -eq $curr_idx ]; then
-                print_selected "$option"
-            else
-                print_option "$option"
-            fi
-            ((idx++))
-        done
-    }
-
-    # initially print empty new lines (scroll down if at bottom of screen)
-    for opt; do printf "\n"; done
-
-    # determine current screen position for overwriting the options
-    local return_value=$1
-    local lastrow=`get_cursor_row`
-    local lastcol=`get_cursor_col`
-    local startrow=$(($lastrow - $#))
-    local startcol=1
-    local lines=$( tput lines )
-    local cols=$( tput cols )
-    local colmax=$2
-    local offset=$(( $cols / $colmax ))
-
-    # local size=$4
-    shift 4
-
-    # ensure cursor and input echoing back on upon a ctrl+c during read -s
-    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
-    cursor_blink_off
-
-    local active_row=0
-    local active_col=0
-    while true; do
-        print_options_multicol $active_col $active_row
-        # user key control
-        case `key_input` in
-            enter)  break;;
-            up)     ((active_row--));
-                    if [ $active_row -lt 0 ]; then active_row=0; fi;;
-            down)   ((active_row++));
-                    if [ $active_row -ge $(( ${#options[@]} / $colmax ))  ]; then active_row=$(( ${#options[@]} / $colmax )); fi;;
-            left)     ((active_col=$active_col - 1));
-                    if [ $active_col -lt 0 ]; then active_col=0; fi;;
-            right)     ((active_col=$active_col + 1));
-                    if [ $active_col -ge $colmax ]; then active_col=$(( $colmax - 1 )) ; fi;;
-        esac
+    local selection
+    selection=$(select_from_menu "Available disks:" "${labels[@]}")
+    local index
+    for i in "${!labels[@]}"; do
+        if [[ "${labels[i]}" == "$selection" ]]; then
+            index="$i"
+            break
+        fi
     done
-
-    # cursor position back to normal
-    cursor_to $lastrow
-    printf "\n"
-    cursor_blink_on
-
-    return $(( $active_col + $active_row * $colmax ))
+    DISK="${disks[$index]}"
+    info "Selected disk: $DISK (${labels[$index]})"
 }
-drivessd () {
-echo -ne "
-Is this an ssd? yes/no:
-"
-
-options=("Yes" "No")
-select_option $? 1 "${options[@]}"
-
-case ${options[$?]} in
-    y|Y|yes|Yes|YES)
-    set_option MOUNT_OPTIONS "noatime,compress=zstd,ssd,commit=120";;
-    n|N|no|NO|No)
-    set_option MOUNT_OPTIONS "noatime,compress=zstd,commit=120";;
-    *) echo "Wrong option. Try again";drivessd;;
-esac
-}
-
-# @description Disk selection for drive to be used with installation.
-diskpart () {
-echo -ne "
-------------------------------------------------------------------------
-    THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK
-    Please make sure you know what you are doing because
-    after formating your disk there is no way to get data back
-------------------------------------------------------------------------
-
-"
-
-PS3='
-Select the disk to install on: '
-options=($(lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print "/dev/"$2"|"$3}'))
-
-select_option $? 1 "${options[@]}"
-disk=${options[$?]%|*}
-
-echo -e "\n${disk%|*} selected \n"
-    set_option DISK ${disk%|*}
-
-drivessd
-}
-diskpart
 # iso=$(curl -4 ifconfig.co/country-iso)
 # timedatectl set-ntp true
 # pacman -S --noconfirm archlinux-keyring # update keyrings to prevent package install failures

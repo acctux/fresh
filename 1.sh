e5d9ea1251224b36b9280267aa2b62c0ac51cbfd
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-03 17:23:25 -0400

    the pipe

--- 1.sh
+++ 1.sh
@@ -2,4 +2,8 @@ pacman -Sy --noconfirm archlinux-keyring
 rm -rf ~/fresh
 pacman -Sy --noconfirm --needed git
 git clone https://github.com/acctux/fresh.git ~/fresh
-exec bash ~/fresh/ark.sh
+echo "Executing ArchTitus Script"
+
+cd $HOME/ArchTitus
+
+exec ./archtitus.sh
